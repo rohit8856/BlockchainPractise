3714a0d8c3271f028ea54045397da5e6eade5f27
Rohit Singh <66157902+rohitya8856@users.noreply.github.com>
2021-06-09 12:01:34 +0530

    some changes

--- Crowd Funding/dao.sol
+++ Crowd Funding/dao.sol
@@ -1,11 +1,15 @@
 
 pragma solidity ^0.8.0;
+import "./funding.sol";
 
 contract dao{
     
-    
-    mapping(address => uint256) public owners;
-    
-    
-    
+    struct projects{
+        string name;
+        uint256 amount;
+        uint256 vote;
+    }
+    uint256 idtoproject;
+     mapping(uint256=> projects) public proj;
+     
 }
